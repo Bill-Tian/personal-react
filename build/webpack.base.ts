c30dbe51b9b4f1240af61d51f783c440687f1188
Mr.T <945119403@qq.com>
2024-04-08 22:34:09 +0800

    [feat]:项目框架构建

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -1,19 +1,15 @@
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from 'path';
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 // const path = require("path");
 console.log(process.env.NODE_ENV);
 
 const cssRegex = /\.css$/;
 const lessRegex = /\.less$/;
-
-const styleLoadersArray = [
-    "style-loader",
-    {
-        loader: "css-loader",
-    },
-];
-
+const styleLoadersArray = ["style-loader", "css-loader", 'postcss-loader'];
 
 const baseConfig: Configuration = {
     entry: path.join(__dirname, '../src/index.tsx'),
@@ -48,6 +44,38 @@ const baseConfig: Configuration = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i, // 匹配图片文件
+                type: "asset", // type选择asset
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 20 * 1024, // 小于10kb转base64
+                    }
+                },
+                generator: {
+                    filename: 'static/images/[hash][ext][query]', // 文件输出目录和命名
+                },
+            },
+            {
+                test: /.(mp4|webm|ogg|mp3|wav|flac|aac)$/, // 匹配媒体文件
+                type: "asset", // type选择asset
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 小于10kb转base64
+                    }
+                },
+                generator: {
+                    filename: 'static/media/[hash][ext][query]', // 文件输出目录和命名
+                },
+            },
+            {
+                test: /\.json$/,// 匹配json文件
+                type: "asset/resource", // 将json文件视为文件类型
+                generator: {
+                    // 这里专门针对json文件的处理
+                    filename: "static/json/[name].[hash][ext][query]",
+                },
+            },
         ]
     },
     resolve: {
@@ -66,6 +94,9 @@ const baseConfig: Configuration = {
                 collapseWhitespace: true,
                 removeComments: true
             }
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
         })
     ]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
 import "@/App.css";
-import lessStyles from "./app.module.less";
+import "./app.less";
+import smallImg from '@/assets/imgs/small.jpg'
+import bigImg from '@/assets/imgs/big.png'
 
 function App() {
   return (
     <div>
       <h2>webpack5-react-ts</h2>
-      <div className={lessStyles["lessBox"]}>
-        <div className={lessStyles["box"]}>lessBox</div>
+      <div className={'lessBox'}>
+        <div className={'box'}>lessBox</div>
       </div>
+      <img src={smallImg} alt="小于10kb的图片" />
+      <img src={bigImg} alt="大于于10kb的图片" />
+      <div className={'smallImg'}>小图片背景</div>
+      <div className={'bigImg'}>大图片背景</div>
+      <div className={'text'}>Optimizing font loading</div>
     </div>
   );
 }
