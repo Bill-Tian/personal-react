48955dd5fd0d50ee2cfc783e310029f19e873e8d
Mr.T <945119403@qq.com>
2024-04-15 22:59:33 +0800

    [feat]:添加背景图中间部分

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setTaskName } from "@/store/features/taskSlice";
+import SvgIcon from "@/components/SvgIcon";
 import "./index.less";
 
 const Home = () => {
@@ -10,9 +11,45 @@ const Home = () => {
     dispath(setTaskName("嘿哈嘿哈"));
   };
 
+  const iconList = [
+    {
+      id: 5,
+      title: "Github",
+      icon: "github",
+    },
+    {
+      id: 6,
+      title: "Email",
+      icon: "email",
+    },
+  ];
+
   return (
     <div>
-      <div className="home-bg"></div>
+      <div>
+        <figure className="home-bg"></figure>
+
+        <div className="welcome">
+          <h2 data-text="HI, Mr.Tian!" className="glitch">
+            HI, Mr.Tian!
+          </h2>
+          <section className="info">
+            <span className="arrow"></span>
+            <p className="descript">我有一壶酒，足以慰风尘，尽倾江海里，赠饮天下人。</p>
+            <div className="top-social">
+              {iconList.map((item) => {
+                return (
+                  <li>
+                    <a href="#" title={item.title}>
+                      <SvgIcon icon={item.icon} size={24} style={{ marginRight: "6px" }} />
+                    </a>
+                  </li>
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      </div>
       <h1>{taskName}</h1>
       <button onClick={setName}>修改名称</button>
       <h2 className="flex m-4 bg-orange-400">webpack5-react-ts2</h2>
